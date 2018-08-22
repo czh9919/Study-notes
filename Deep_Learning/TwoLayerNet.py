b477cf41bd402d789bd131f64e8f08712d207569
czh9919 <czh9919@outlook.com>
2018-08-22 20:06:00 +0800

    update

--- Deep_Learning/TwoLayerNet.py
+++ Deep_Learning/TwoLayerNet.py
@@ -1,14 +1,16 @@
 import sys,os
 sys.path.append(os.path.abspath('F:/notes'))
+import numpy as np
 from common.functions import *
 from common.gradient import numerical_gradient
 class TwoLayerNet:
-    def __init__(self,input_size,hidden_size,ouput_size,weight_init_std=0.01)
+    def __init__(self,input_size,hidden_size,output_size,weight_init_std=0.01):
         self.params={} #用于保存权重
         self.params['W1']=weight_init_std * np.random.randn(input_size,hidden_size)
         self.params['b1']=np.zeros(hidden_size)
         self.params['W2']=weight_init_std * np.random.randn(hidden_size,ouput_size)
         self.params['b2']=np.zeros(output_size)
+
     def predict(self,x):
         W1,W2=self.params['W1'],self.params['W2']
         b1,b2=self.params['b1'],self.params['b2']
@@ -27,7 +29,7 @@ class TwoLayerNet:
     def accuracy(self,x,t):
         y=self.predict(x)
         y=np.argmax(y,axis=1)
-        t=argmax(t,axis=1)
+        t=np.argmax(t,axis=1)
 
         accuracy=np.sum(y==t)/float(x.shape[0])
         return accuracy
